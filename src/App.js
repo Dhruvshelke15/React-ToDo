751664981211c780093dd276042c26852d7e6a8f
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-10-12 12:48:38 +0530

    create basic interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
+};
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>To Do App</h3>
+        <form onSubmit="" className={style.form}>
+          <input
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            className={style.input}
+            type="text"
+            placeholder="Add Todo"
+          />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
